2b870f3a7855252ddcf5f72527c3098ada77abcd
= <=>
2016-07-20 15:38:17 +0800

    updating

--- src/main/java/demo/App.java
+++ src/main/java/demo/App.java
@@ -6,8 +6,21 @@ package demo;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public static void main(String[] args)
+	{
+		Pet pet = new Pet("Peter");
+		pet.bark();
+	}
+}
+class Pet
+{
+	String name;
+	public Pet(String name)
+	{
+		System.out.println("Pet name is " + name);
+	}
+	public void bark()
+	{
+		System.out.println("Peter says aw aw!");
+	}
 }
